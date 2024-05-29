441b0f397f7bbca71342d87cd14856a377978a75
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-29 17:23:09 +0300

    add crud files production.py and recipe.py

--- app/crud/production.py
+++ app/crud/production.py
@@ -7,17 +7,31 @@ from exceptions import DataBaseConnectionError
 
 from models import Production
 
+from . import product as crud_product
+
 
 
 async def create_production(
     session: AsyncSession,
     production_create: ProductionCreateSchema,
+    product_id: int,
     
     ) -> Production:
     
     production = Production(
         **production_create,
     )
+
+    product = crud_product.get_proudct_by_id(
+        session=session,
+        product_id=product_id,
+        )
+
+    #add services func for add stocks of production
+    #this info we can get from receptyre file which we need create.
+    
+    production.products.append(product)
+    # production.stocks_m2m.extend(stocks)
 
     # add products and stocks here for m2m relationship
 
