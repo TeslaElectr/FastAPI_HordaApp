1307a3a1d0a0fa828ca2bb642ff6b881cbf42e97
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-29 23:26:33 +0300

    add if name==main in main_crud

--- app/main_crud.py
+++ app/main_crud.py
@@ -1,3 +1,4 @@
+import asyncio
 
 from models import Product
 from models import Company
@@ -8,6 +9,16 @@ from models import Recipe
 
 from crud import company as crud_company
 
+from db import db_conn as db_helper
+
+
+async def main():
+    async with db_helper.create_session() as session:
+        pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 
