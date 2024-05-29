f3b71a151e7c2a721d87eb7a21471942d100c5ed
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-29 14:29:51 +0300

    make migrations, add fields in Production models

---
+++ app/api/v1/endpoints/productions.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter
+from fastapi import Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db import session_dependency
+
+from models import Production
+
+from schemas import ProductionCreateSchema
+from schemas import ProductionSchema
+
+from crud import production as crud_production
+
+router = APIRouter(
+    prefix="/productions",
+    tags=["Production"],
+)
+
+
+@router.post("/", response_model=ProductionSchema)
+async def add_production(
+    create_production: ProductionCreateSchema,
+    session: AsyncSession = Depends(session_dependency),
+    ) -> Production:
+    
+    production = await crud_production.create_production(
+        session=session,
+        production_create=create_production,
+    )
+
+    return production
+    
+    
