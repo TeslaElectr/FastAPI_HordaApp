026c0e220593f361acc4ce285a8a5df5e1b38ba7
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-31 12:31:53 +0300

    add data for upload and test db

--- app/cfg_company.py
+++ app/cfg_company.py
@@ -21,53 +21,53 @@ def get_list_companies():
         email="amdor@ucp.ru",
     )
 
-    amdor = CompanyCreateSchema(
-        name="Amdor",
-        city="N-Tagil",
-        inn=6623024667,
-        kpp=662301001,
-        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
-        phone=75555555555,
-        email="amdor@ucp.ru",
+    sinteztnp = CompanyCreateSchema(
+        name="SintezTNP",
+        city="Ufa",
+        inn=277003925,
+        kpp=27701001,
+        address="450029, RESPUBLIKA BASHKORTOSTAN, G. UFA, UL. YUBILEJNAYA, D.5",
+        phone=72222222222,
+        email="sintez@tnp.ru",
     )
 
-    amdor = CompanyCreateSchema(
-        name="Amdor",
-        city="N-Tagil",
-        inn=6623024667,
-        kpp=662301001,
-        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
-        phone=75555555555,
-        email="amdor@ucp.ru",
+    diks = CompanyCreateSchema(
+        name="Diks",
+        city="Saint-Petersburg",
+        inn=7802327116,
+        kpp=780601001,
+        address="195030, G.SANKT-PETERBURG, UL. HIMIKOV, D. 28, LITER AS, POMESHCH. 906",
+        phone=77777777777,
+        email="info@diks-spb.ru",
     )
 
-    amdor = CompanyCreateSchema(
-        name="Amdor",
-        city="N-Tagil",
-        inn=6623024667,
-        kpp=662301001,
-        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
-        phone=75555555555,
-        email="amdor@ucp.ru",
+    euroline = CompanyCreateSchema(
+        name="EurolineTrading",
+        city="Saint-Petersburg",
+        inn=7805450679,
+        kpp=784201001,
+        address="g. Sankt-Peterburg, ul. SHpalernaya, d. 51, lit. A, pom. 2-N, kom. 296",
+        phone=73333333333,
+        email="info@etldr.ru",
     )
-    amdor = CompanyCreateSchema(
-        name="Amdor",
-        city="N-Tagil",
-        inn=6623024667,
-        kpp=662301001,
-        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
-        phone=75555555555,
-        email="amdor@ucp.ru",
+    itps = CompanyCreateSchema(
+        name="ITPS",
+        city="Kazan",
+        inn=1660043700,
+        kpp=166001001,
+        address="g. Kazan', ul. Nikolaya Ershova, d. 35a, pom. 46-50, et. 2",
+        phone=74444444444,
+        email="kazan@itps.ru",
     )
     
-    amdor = CompanyCreateSchema(
-        name="Amdor",
-        city="N-Tagil",
-        inn=6623024667,
-        kpp=662301001,
-        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
-        phone=75555555555,
-        email="amdor@ucp.ru",
+    omea = CompanyCreateSchema(
+        name="Omea Algol rus",
+        city="Moscow",
+        inn=7816108883,
+        kpp=770501001,
+        address="g. Moskva, vn.ter.g. municipal'nyj okrug Zamoskvorech'e, Kosmodamianskaya nab., d. 52, str. 4",
+        phone=71111111111,
+        email="111@omea.ru",
     )
 
-    return [horda,amdor]
+    return [horda, amdor, sinteztnp, diks, euroline, itps, omea]
---
+++ app/cfg_products.py
@@ -0,0 +1,9 @@
+from schemas import ProductCreateSchema2
+from schemas import ProductCreateSchema
+
+
+rb265 = ProductCreateSchema2(
+    name="RoadBase 265",
+    description="adgezive additive for sintez road chemical",
+    company_id=...,
+    )
