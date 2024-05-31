3b6771a493a055f3a339e4cab3000a53d506c602
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-31 12:31:53 +0300

    add crud settings

---
+++ app/cfg_company.py
@@ -0,0 +1,73 @@
+from schemas import CompanyCreateSchema
+
+def get_list_companies():
+    horda = CompanyCreateSchema(
+        name="Horda",
+        city="Saint-Petersburg",
+        inn=7813299598,
+        kpp=781301001,
+        address="197022, gorod Sankt-Peterburg, ul CHapygina, d. 6 litera P, ofis 214, r.m. 1",
+        phone=79999999999,
+        email="ivan_lo@hordanpp.com"
+    )
+
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+    
+    amdor = CompanyCreateSchema(
+        name="Amdor",
+        city="N-Tagil",
+        inn=6623024667,
+        kpp=662301001,
+        address="622051, Sverdlovskaya oblast', gorod Nizhnij Tagil, Severnoe sh., d.21",
+        phone=75555555555,
+        email="amdor@ucp.ru",
+    )
+
+    return [horda,amdor]
