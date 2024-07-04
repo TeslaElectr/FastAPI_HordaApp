e3831cd62053690f355831ee4200ba2dbbba70e8
Ivan Lebedev <ivanleb171@gmail.com>
2024-07-04 13:41:53 +0300

    add app/db/database.DatabasesessionManager

---
+++ app/db/database.py
@@ -0,0 +1,68 @@
+import contextlib
+from collections.abc import AsyncIterator
+
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncConnection
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from models import Base
+
+
+
+class DatabaseSessionManager:
+    
+    def __init__(self):
+        self._engine: AsyncEngine | None = None
+        self._sessionmaker: async_sessionmaker | None = None
+        
+    def init(self, host):
+        self._engine = create_async_engine(host)
+        self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine)
+
+        
+    async def close(self):
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is nor initialized")
+
+        await self._engine.dispose()
+        self._engine = None
+        self._sessionmaker = None
+    
+    
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[AsyncConnection]:
+        if self._engine is None:
+            raise Exception("DatabaseSessionManager is nor initialized")
+
+        async with self._engine.begin() as connection: 
+            try: 
+                yield connection
+            except Exception:
+                await connection.rollback()
+                raise
+
+            
+    @contextlib.asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncSession]:
+        if self._sessionmaker is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+
+        session = self._sessionmaker()
+        
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+            
+    async def create_all(self, connection: AsyncConnection):
+        await connection.run_sync(Base.metadata.create_all)
+
+    async def drop_all(self, connection: AsyncConnection):
+        await connection.run_sync(Base.metadata.drop_all)
+            
